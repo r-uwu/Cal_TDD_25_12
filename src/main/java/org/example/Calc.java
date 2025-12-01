f167010f4207678cc8361f73bfd7a9fbbe6bfab5
r-uwu <yarrow.uwu@gmail.com>
2025-12-01 14:51:32 +0900

    10 - 10 - 10 - 10 + 10 + 10 + 10

--- src/main/java/org/example/Calc.java
+++ src/main/java/org/example/Calc.java
@@ -7,58 +7,22 @@ public class Calc {
 //        String[] bits = exp.split(" \\+ ");
 
         String[] bits = exp.split(" ");
-        int result = 0;
+        int result = Integer.parseInt(bits[0]);
+        String op;
 
-        for(int n = 0; n < bits.length; n++){
-            if(bits[n].equals("+")){
-                if(result == 0)
-                result = sum(Integer.parseInt(bits[n-1]), Integer.parseInt(bits[n+1]));
-                else
-                    result = result + Integer.parseInt(bits[n+1]);
-            }
-            else if(bits[n].equals("-")){
-                if(result == 0)
-                result = sub(Integer.parseInt(bits[n-1]), Integer.parseInt(bits[n+1]));
-                else
-                    result = result - Integer.parseInt(bits[n+1]);
-            }
-            else continue;
+        for(int n = 1; n < bits.length; n+=2){
+            op = bits[n];
+
+            if(op.equals("+"))
+                result+= Integer.parseInt(bits[n+1]);
+            else if(op.equals("-"))
+                result-=Integer.parseInt(bits[n+1]);
+            else if(op.equals("*"))
+                result*=Integer.parseInt(bits[n+1]);
+            else if(op.equals("/"))
+                result/=Integer.parseInt(bits[n+1]);
         }
-        /*
-        boolean needToPlus = exp.contains("+");
-        boolean needToMinus = exp.contains("-");
-
-        String[] bits = null;
-
-        if (needToPlus) {
-            bits = exp.split(" \\+ ");
-        } else if (needToMinus) {
-            bits = exp.split(" \\- ");
-        }
-
-        int a = Integer.parseInt(bits[0]);
-        int b = Integer.parseInt(bits[1]);
-
-        if (needToPlus) {
-            return (sum(a ,b));
-        }
-        else if (needToMinus) {
-            return (sub(a ,b));
-        }
-
-        throw new RuntimeException("해석 불가 : 올바른 계산식이 아님");
-        */
-
         return result;
     }
-
-    static int sum(int a, int b) {
-        return a + b;
-    }
-
-    static int sub(int a, int b) {
-        return a - b;
-    }
-
 
 }
