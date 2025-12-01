e42d5c8909c93313627b5d119f16a49a77dd33db
r-uwu <yarrow.uwu@gmail.com>
2025-12-01 12:32:54 +0900

    1 + 1

---
+++ src/main/java/org/example/Calc.java
@@ -0,0 +1,22 @@
+package org.example;
+
+public class Calc {
+    public static int run(String exp){
+
+        String[] bits = exp.split(" ");
+
+        int a = Integer.parseInt(bits[0]);
+        int b = Integer.parseInt(bits[2]);
+
+        if(bits[1].equals("+"))
+        {
+            return a+b;
+        }
+        else if(bits[1].equals("-"))
+        {
+            return a-b;
+        }
+        else return 0;
+
+    }
+}
---
+++ src/test/java/org/example/CalcTest.java
@@ -0,0 +1,25 @@
+package org.example;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class CalcTest {
+
+
+    @Test
+    @DisplayName("1 + 1 = 2")
+    public void test1() {
+        assertThat(Calc.run("1 + 1")).isEqualTo(2);
+    }
+
+    @Test
+    @DisplayName("2 + 1 = 3")
+    public void test2() {
+        assertThat(Calc.run("2 + 1")).isEqualTo(3);
+    }
+
+
+
+}
