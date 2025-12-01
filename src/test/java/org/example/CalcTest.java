47af23829b5826603e4b7f91c0b93067835eedbf
r-uwu <yarrow.uwu@gmail.com>
2025-12-01 14:39:38 +0900

    10 - 10 - 10 - 10 + 10 + 10 + 10

--- src/test/java/org/example/CalcTest.java
+++ src/test/java/org/example/CalcTest.java
@@ -37,4 +37,12 @@ public class CalcTest {
     public void test5() {
         assertThat(Calc.run("10 - 20 + 30")).isEqualTo(20);
     }
+
+    @Test
+    @DisplayName("10 - 10 - 10 - 10 + 10 + 10 + 10")
+    public void test6() {
+        assertThat(Calc.run("10 - 10 - 10 - 10 + 10 + 10 + 10")).isEqualTo(10);
+    }
+
+
 }
