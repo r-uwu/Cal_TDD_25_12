3149aa935e93855e0833db0ee4db0a5e2d6b88df
r-uwu <yarrow.uwu@gmail.com>
2025-12-04 09:24:34 +0900

    re-push

---
+++ src/main/java/org/example/Calc.java
@@ -0,0 +1,30 @@
+package org.example;
+
+public class Calc {
+    public static int run(String exp) {
+
+        String[] bits = exp.split(" ");
+        String op;
+
+        for(int i = 1; i < bits.length; i++) {
+            op = bits[i];
+
+            if (op.equals("*"))
+                bits[i - 1] = String.valueOf((Integer.parseInt(bits[i - 1]) * Integer.parseInt(bits[i + 1])));
+            else if (op.equals("/"))
+                bits[i - 1] = String.valueOf((Integer.parseInt(bits[i - 1]) / Integer.parseInt(bits[i + 1])));
+        }
+        int result = Integer.parseInt(bits[0]);
+
+        for(int n = 1; n < bits.length; n+=2){
+            op = bits[n];
+
+            if(op.equals("+"))
+                result+= Integer.parseInt(bits[n+1]);
+            else if(op.equals("-"))
+                result-=Integer.parseInt(bits[n+1]);
+        }
+        return result;
+    }
+
+}
